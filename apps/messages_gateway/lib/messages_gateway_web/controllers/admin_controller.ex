38df350fc37f272ad0e9ddc8626ac401d717ebb3
ruslan moroz <r.moroz@skywell.software>
2018-12-04 15:29:13 +0200

    EH-57:added API params check, added uuid and priority, added Jason lib

--- apps/messages_gateway/lib/messages_gateway_web/controllers/admin_controller.ex
+++ apps/messages_gateway/lib/messages_gateway_web/controllers/admin_controller.ex
@@ -15,9 +15,9 @@ defmodule MessagesGatewayWeb.AdminController do
     end
   end
 
-  def add_system_config(conn, %{"resource" => body}) do
+  def add_system_config(conn, %{"resource" => %{"auth" => auth, "password" => password}}) do
     case :ok do # mockup
-      # case DbAgent.add_system_config(body) do
+      # case DbAgent.add_system_config(auth, password) do
       :ok ->
         render(conn, "index.json",
           %{:body => %{
@@ -29,9 +29,13 @@ defmodule MessagesGatewayWeb.AdminController do
     end
   end
 
-  def edit_system_config(conn, %{"resource" => body}) do
+  def add_system_config(conn,  _) do
+    render(conn, "index.json", %{:body => %{:status => "error", :message => "Missed some request params"}})
+  end
+
+  def edit_system_config(conn, %{"resource" => %{"auth" => auth, "password" => password}}) do
     case :ok do # mockup
-      # case DbAgent.edit_system_config(body) do
+      # case DbAgent.edit_system_config(auth, password) do
       :ok ->
         render(conn, "index.json",
           %{:body => %{
@@ -41,6 +45,10 @@ defmodule MessagesGatewayWeb.AdminController do
       {:error, error} ->
         render(conn, "index.json", %{:body => %{:status => "error", :message => error}})
     end
+  end
+
+  def edit_system_config(conn,  _) do
+    render(conn, "index.json", %{:body => %{:status => "error", :message => "Missed some request params"}})
   end
 
   def get_operator_types(conn, _params) do
@@ -56,9 +64,9 @@ defmodule MessagesGatewayWeb.AdminController do
     end
   end
 
-  def add_operator_type(conn, %{"resource" => body}) do
+  def add_operator_type(conn, %{"resource" => %{"name" => name}}) do
     case :ok do # mockup
-      # case DbAgent.add_operator_type(body) do
+      # case DbAgent.add_operator_type(name) do
       :ok ->
         render(conn, "index.json",
           %{:body => %{
@@ -70,9 +78,13 @@ defmodule MessagesGatewayWeb.AdminController do
     end
   end
 
-  def delete_operator_type(conn, %{"resource" => body}) do
+  def add_operator_type(conn,  _) do
+    render(conn, "index.json", %{:body => %{:status => "error", :message => "Missed some request params"}})
+  end
+
+  def delete_operator_type(conn, %{"resource" => %{"name" => name}}) do
     case :ok do # mockup
-      # case DbAgent.delete_operator_type(body) do
+      # case DbAgent.delete_operator_type(name) do
       :ok ->
         render(conn, "index.json",
           %{:body => %{
@@ -82,6 +94,10 @@ defmodule MessagesGatewayWeb.AdminController do
       {:error, error} ->
         render(conn, "index.json", %{:body => %{:status => "error", :message => error}})
     end
+  end
+
+  def delete_operator_type(conn,  _) do
+    render(conn, "index.json", %{:body => %{:status => "error", :message => "Missed some request params"}})
   end
 
   def get_all_operators(conn, _params) do
@@ -99,9 +115,9 @@ defmodule MessagesGatewayWeb.AdminController do
     end
   end
 
-  def add_operator(conn, %{"resource" => body}) do
+  def add_operator(conn, %{"resource" => %{"name" => name, "limit" => limit, "price" => price, "config" => config}}) do
     case :ok do # mockup
-      # case DbAgent.add_operator(body) do
+      # case DbAgent.add_operator(name, limit, price, config) do
       :ok ->
         render(conn, "index.json",
           %{:body => %{
@@ -113,9 +129,13 @@ defmodule MessagesGatewayWeb.AdminController do
     end
   end
 
-  def operator_edit(conn, %{"resource" => body}) do
+  def add_operator(conn,  _) do
+    render(conn, "index.json", %{:body => %{:status => "error", :message => "Missed some request params"}})
+  end
+
+  def operator_edit(conn, %{"resource" => %{"name" => name, "limit" => limit, "price" => price, "config" => config}}) do
     case :ok do # mockup
-      # case DbAgent.operator_edit(body) do
+      # case DbAgent.operator_edit(name, limit, price, config) do
       :ok ->
         render(conn, "index.json",
           %{:body => %{
@@ -127,9 +147,13 @@ defmodule MessagesGatewayWeb.AdminController do
     end
   end
 
-  def operator_delete(conn, %{"resource" => body}) do
+  def operator_edit(conn,  _) do
+    render(conn, "index.json", %{:body => %{:status => "error", :message => "Missed some request params"}})
+  end
+
+  def operator_delete(conn, %{"resource" => %{"operator_id" => operator_id}}) do
     case :ok do # mockup
-      # case DbAgent.operator_delete(body) do
+      # case DbAgent.operator_delete(operator_id) do
       :ok ->
         render(conn, "index.json",
           %{:body => %{
@@ -141,51 +165,94 @@ defmodule MessagesGatewayWeb.AdminController do
     end
   end
 
-  def send_message(conn, %{"resource" => body}) do
+  def operator_delete(conn,  _) do
+    render(conn, "index.json", %{:body => %{:status => "error", :message => "Missed some request params"}})
+  end
+
+  def send_message(conn, %{"resource" => %{"request_id" => request_id, "contact" => contact, "body" => body} = resource}) do
+    uuid = MessagesGateway.UUID.generate_uuid()
+    priority_list = MessagesGateway.DbAgent.get_priority_list()
+    body = Jason.encode!(Map.merge(resource, %{"uuid" => uuid, "priority_list" => priority_list}))
     case :ok do # mockup
       # case MessagesGateway.MqPublisher.publish(body) do
       :ok ->
-    render(conn, "index.json",
-      %{:body => %{
-        :meta => %{:url => "https://localhost:4000", :type => "list", :code => "200",:idempotency_key => "iXXekd88DKqo", :request_id => "qudk48fFlaP"},
-        :data => %{:status => "ok"}}}
-    )
+        case :ok do # mockup
+       # case MessagesGateway.RedisMannager.put(uuid, "true", "false") do  TODO RedisMannager
+          :ok ->
+            render(conn, "index.json",
+              %{:body => %{
+                :meta => %{:url => "https://localhost:4000", :type => "list", :code => "200",:idempotency_key => "iXXekd88DKqo", :request_id => "qudk48fFlaP"},
+                :data => %{:status => "ok"}}}
+            )
+          :error ->
+            render(conn, "index.json", %{:body => %{:status => "error", :message => "Redis error"}})
+        end
       {:error, error} ->
         render(conn, "index.json", %{:body => %{:status => "error", :message => error}})
     end
   end
 
-  def send_sms(conn, %{"resource" => body}) do
-    case :ok do # mockup
-      # case MessagesGateway.MqPublisher.publish(body) do
+  def send_message(conn,  _) do
+    render(conn, "index.json", %{:body => %{:status => "error", :message => "Missed some request params"}})
+  end
+
+  def send_sms(conn, %{"resource" => %{"request_id" => request_id, "phone" => phone, "body" => body} = resource}) do
+    uuid = MessagesGateway.UUID.generate_uuid()
+    priority_list = [1] #only sms
+    body = Jason.encode!(Map.merge(resource, %{"uuid" => uuid, "priority_list" => priority_list}))
+  #  case :ok do # mockup
+       case MessagesGateway.MqPublisher.publish(body) do
       :ok ->
-    render(conn, "index.json",
-      %{:body => %{
-        :meta => %{:url => "https://localhost:4000", :type => "list", :code => "200",:idempotency_key => "iXXekd88DKqo", :request_id => "qudk48fFlaP"},
-        :data => %{:status => "ok"}}}
-    )
+        case :ok do # mockup
+          # case MessagesGateway.RedisMannager.put(uuid, "true", "false") do  TODO RedisMannager
+          :ok ->
+            render(conn, "index.json",
+              %{:body => %{
+                :meta => %{:url => "https://localhost:4000", :type => "list", :code => "200",:idempotency_key => "iXXekd88DKqo", :request_id => "qudk48fFlaP"},
+                :data => %{:status => "ok"}}}
+            )
+          :error ->
+            render(conn, "index.json", %{:body => %{:status => "error", :message => "Redis error"}})
+        end
       {:error, error} ->
         render(conn, "index.json", %{:body => %{:status => "error", :message => error}})
     end
   end
 
-  def send_email(conn, %{"resource" => body}) do
+  def send_sms(conn,  _) do
+    render(conn, "index.json", %{:body => %{:status => "error", :message => "Missed some request params"}})
+  end
+
+  def send_email(conn, %{"resource" => %{"request_id" => request_id, "email" => email, "body" => body} = resource}) do
+    uuid = MessagesGateway.UUID.generate_uuid()
+    priority_list = [2] #only email
+    body = Jason.encode!(Map.merge(resource, %{"uuid" => uuid, "priority_list" => priority_list}))
     case :ok do # mockup
       # case MessagesGateway.MqPublisher.publish(body) do
       :ok ->
-    render(conn, "index.json",
-      %{:body => %{
-        :meta => %{:url => "https://localhost:4000", :type => "list", :code => "200",:idempotency_key => "iXXekd88DKqo", :request_id => "qudk48fFlaP"},
-        :data => %{:status => "ok"}}}
-    )
+        case :ok do # mockup
+          # case MessagesGateway.RedisMannager.put(uuid, "true", "false") do  TODO RedisMannager
+          :ok ->
+            render(conn, "index.json",
+              %{:body => %{
+                :meta => %{:url => "https://localhost:4000", :type => "list", :code => "200",:idempotency_key => "iXXekd88DKqo", :request_id => "qudk48fFlaP"},
+                :data => %{:status => "ok"}}}
+            )
+          :error ->
+            render(conn, "index.json", %{:body => %{:status => "error", :message => "Redis error"}})
+        end
       {:error, error} ->
         render(conn, "index.json", %{:body => %{:status => "error", :message => error}})
     end
   end
 
-  def message_status(conn, %{"resource" => body}) do
+  def send_email(conn,  _) do
+    render(conn, "index.json", %{:body => %{:status => "error", :message => "Missed some request params"}})
+  end
+
+  def message_status(conn, %{"resource" => %{"request_id" => request_id}}) do
     case {:ok, %{:status => "sending"}} do # mockup
-      # case MessagesRouter.message_status() do
+      # case MessagesRouter.message_status(request_id) do
       {:ok, response} ->
         render(conn, "index.json",
           %{:body => %{
@@ -197,12 +264,8 @@ defmodule MessagesGatewayWeb.AdminController do
     end
   end
 
-  def wait_for_messages do
-    receive do
-      {:basic_deliver, payload, _meta} ->
-        IO.puts " [x] Received #{payload}"
-        wait_for_messages()
-    end
+  def message_status(conn,  _) do
+    render(conn, "index.json", %{:body => %{:status => "error", :message => "Missed some request params"}})
   end
 
 end
