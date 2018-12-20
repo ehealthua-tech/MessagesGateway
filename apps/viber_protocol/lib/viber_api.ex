26930572e9e8394ea5e5d611dcdd1aef11f2ba8e
a.savenkov <a.savenkov@skywell.software>
2018-12-20 14:17:38 +0200

    EH-71: add webhook

--- apps/viber_protocol/lib/viber_api.ex
+++ apps/viber_protocol/lib/viber_api.ex
@@ -10,10 +10,9 @@ defmodule ViberApi do
 
   end
 
-  def send_message(phone, message) do
-    message
-    |> Enum.filter(fn {_, v} -> v end)
-    |> Enum.into(%{})
+  def add_contact(conn) do
+    :io.format("~n~nconn :~p~n", [conn])
+    :io.format("~n~nconn :~p~n", [conn.body_params])
     :noreply
   end
 
