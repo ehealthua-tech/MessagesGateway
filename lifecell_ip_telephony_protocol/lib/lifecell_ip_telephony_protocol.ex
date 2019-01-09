da267b92a6c9438d977b916273d8c2a12bbd6a8a
a.savenkov <a.savenkov@skywell.software>
2019-01-09 16:39:09 +0200

    EH-73: create protocol

---
+++ lifecell_ip_telephony_protocol/lib/lifecell_ip_telephony_protocol.ex
@@ -0,0 +1,18 @@
+defmodule LifecellIpTelephonyProtocol do
+  @moduledoc """
+  Documentation for LifecellIpTelephonyProtocol.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LifecellIpTelephonyProtocol.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ lifecell_ip_telephony_protocol/lib/lifecell_ip_telephony_protocol/application.ex
@@ -0,0 +1,20 @@
+defmodule LifecellIpTelephonyProtocol.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      # Starts a worker by calling: LifecellIpTelephonyProtocol.Worker.start_link(arg)
+      # {LifecellIpTelephonyProtocol.Worker, arg},
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: LifecellIpTelephonyProtocol.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
