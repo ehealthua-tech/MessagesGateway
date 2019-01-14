7ecb2364eb5d254b005e093e4ab48a0a708b7927
ruslan moroz <r.moroz@skywell.software>
2019-01-14 15:42:17 +0200

    EH-107:moved test database config

--- apps/db_agent/config/dev.exs
+++ apps/db_agent/config/dev.exs
@@ -8,11 +8,3 @@ config :db_agent, DbAgent.Repo,
        database: "messages_gateway",
        hostname: "localhost",
        pool_size: 10
-
-config :db_agent, DbAgent.Repo,
-       adapter: Ecto.Adapters.Postgres,
-       username: "postgres",
-       password: "postgres",
-       database: "messages_gateway_test",
-       hostname: "localhost",
-       pool_size: 10
--- apps/db_agent/config/test.exs
+++ apps/db_agent/config/test.exs
@@ -2,9 +2,9 @@ use Mix.Config
 
 # Configure your database
 config :db_agent, DbAgent.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "db_agent_test",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
+       adapter: Ecto.Adapters.Postgres,
+       username: "postgres",
+       password: "postgres",
+       database: "messages_gateway_test",
+       hostname: "localhost",
+       pool_size: 10
