4903653137beb7392b575356cbdcda8d6a9db169
ruslan moroz <r.moroz@skywell.software>
2019-01-31 13:06:53 +0200

    EH-118:add get all keys rest

--- apps/messages_gateway/lib/messages_gateway_web/views/keys_view.ex
+++ apps/messages_gateway/lib/messages_gateway_web/views/keys_view.ex
@@ -2,12 +2,12 @@ defmodule MessagesGatewayWeb.KeysView do
   @moduledoc false
   use MessagesGatewayWeb, :view
 
-  def render("index.json", %{:user => user, :key => key, :status => status}) do
-    %{user: user, key: key, status: status}
-  end
-
   def render("change_keys.json", %{:status => status}) do
     %{status: status}
+  end
+
+  def render("keys.json", %{:keys => keys}) do
+    %{keys: keys}
   end
 
 end
