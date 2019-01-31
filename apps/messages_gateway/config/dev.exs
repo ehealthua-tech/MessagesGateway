840408140b2d58f7ad7144ecd32648fbc4a7817f
a.savenkov <a.savenkov@skywell.software>
2019-01-31 15:30:40 +0200

    EH-121: fix config

--- apps/messages_gateway/config/dev.exs
+++ apps/messages_gateway/config/dev.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
 config :messages_gateway, MessagesGatewayWeb.Endpoint,
-  http: [port: 4001],
+  http: [port: 4011],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
@@ -39,6 +39,7 @@ config :messages_gateway, MessagesGatewayWeb.Endpoint,
       ~r{lib/messages_gateway_web/templates/.*(eex)$}
     ]
   ]
+  
 
 # Do not include metadata nor timestamps in development logs
 config :logger, :console, format: "[$level] $message\n"
