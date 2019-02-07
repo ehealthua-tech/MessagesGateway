08abbc31711535bd3cf18cf1d8c94256d658d026
ruslan moroz <r.moroz@skywell.software>
2019-02-07 11:16:10 +0200

    db_agent tests changed

--- apps/db_agent/test/db_agent/db_agent_test.exs
+++ apps/db_agent/test/db_agent/db_agent_test.exs
@@ -14,11 +14,11 @@ defmodule DbAgent.DbAgentTest do
   """
   test "database_test" do
     assert [] == DbAgent.OperatorTypesRequests.list_operator_types()
-    new_operator_type = %{"active" => :true, "name" => "type_name", "priority" => 7}
+    new_operator_type = %{:active => :true, :name => "type_name"}
     {:ok, created_operator_type} = DbAgent.OperatorTypesRequests.add_operator_type(new_operator_type)
-    assert %{active: true, name: "type_name", priority: 7} ==
+    assert %{active: true, name: "type_name", priority: 1} ==
              %{active: created_operator_type.active, name: created_operator_type.name, priority: created_operator_type.priority}
-    assert {1, nil} == DbAgent.OperatorTypesRequests.change_status(%{:id => created_operator_type.id, :active => :false, :name => "type_name", :priority => 7})
+    assert {1, nil} == DbAgent.OperatorTypesRequests.change_status(%{:id => created_operator_type.id, :active => :false, :name => "type_name", "priority" => 1})
     DbAgent.OperatorTypesRequests.update_priority([%{"id" => created_operator_type.id, "priority" => 12, "active" => :false}])
     [updated_operator_type] = DbAgent.OperatorTypesRequests.list_operator_types()
     assert %{active: false, name: "type_name", priority: 12} ==
@@ -27,15 +27,17 @@ defmodule DbAgent.DbAgentTest do
 
 
     assert [] == DbAgent.OperatorsRequests.list_operators()
-    new_operator = %{"name" => "viber_test", "config" => %{"bla" => "bla"}, "priority" => 2, "price" => 12, "protocol_name" => "viber_protocol",
+    new_operator = %{"name" => "viber_test", "config" => %{"bla" => "bla"}, "price" => 12, "protocol_name" => "viber_protocol",
       "limit" => 100, "operator_type_id" => created_operator_type.id, "active" => :true}
     {:ok, created_operator} = DbAgent.OperatorsRequests.add_operator(new_operator)
     DbAgent.OperatorsRequests.get_by_name(created_operator.name)
     {1, nil} = DbAgent.OperatorsRequests.change_operator(created_operator.id, [id: created_operator.id,
-      name: "viber_test", config: %{"bla" => "bla"}, priority: 2, price: 14, limit: 100, protocol_name: "viber_protocol", active: :true])
+      name: "viber_test", config: %{"bla" => "bla"}, price: 14, limit: 100, protocol_name: "viber_protocol", active: :true])
     DbAgent.OperatorsRequests.operator_by_id(created_operator.id)
     DbAgent.OperatorsRequests.update_priority([%{"id" => created_operator_type.id, "priority" => 12, "active" => :false}])
+    DbAgent.OperatorsRequests.update_priority([%{id: created_operator_type.id, priority: 12, active: :false}])
     DbAgent.OperatorsRequests.operator_by_operator_type_id(created_operator_type.id)
+    DbAgent.OperatorsRequests.calc_priority(%{"name" => "test", "price" => 12})
 
 
     assert [] == DbAgent.ContactsRequests.list_contacts()
