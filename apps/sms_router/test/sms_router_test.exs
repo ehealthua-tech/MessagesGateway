e928d1b7d0874207cbc778a49a835d7ce6155975
ruslan moroz <r.moroz@skywell.software>
2019-02-07 17:01:51 +0200

    EH-66:sms_router tests

--- apps/sms_router/test/sms_router_test.exs
+++ apps/sms_router/test/sms_router_test.exs
@@ -3,12 +3,22 @@ defmodule SmsRouterTest do
   alias SmsRouter.RedisManager
   doctest SmsRouter
 
-  test "greets the world" do
+  @sys_config %{default_sms_operator: "", org_name: "test", sending_time: "60", automatic_prioritization: false,
+    messages_router_module: __MODULE__, messages_router_method: "send"}
+
+  test "sms_router" do
 
     message_id = "test_id678"
-    RedisManager.set(message_id, %{message_id: message_id, contact: "+380632688304",
+    RedisManager.set("system_config", @sys_config)
+    RedisManager.set(message_id, %{message_id: message_id, contact: "+380632688394",
       body: "test message", callback_url: "", priority_list: [], active: true, sending_status: "in_queue"})
 
     assert :ok == SmsRouter.check_and_send(%{contact: "+380632688304", body: "test message", message_id: message_id})
+    RedisManager.del("system_config")
+    RedisManager.keys("system_config")
+    RedisManager.del("system_config1")
   end
+
+  def send(_), do: :ok
+
 end
