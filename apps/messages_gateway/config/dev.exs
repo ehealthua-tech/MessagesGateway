37237b6a177b6b8ae7dabbd69552d0610ae8fb15
a.savenkov <a.savenkov@skywell.software>
2019-02-08 14:10:54 +0200

    fix config

--- apps/messages_gateway/config/dev.exs
+++ apps/messages_gateway/config/dev.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
 config :messages_gateway, MessagesGatewayWeb.Endpoint,
-  http: [port: 4011],
+  http: [port: 4001],
   debug_errors: false,
   code_reloader: true,
   check_origin: false,
