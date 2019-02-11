73e576934c1fb04d1a44a1e8e44f3a9fb6e56cc6
ruslan moroz <r.moroz@skywell.software>
2019-02-11 14:44:39 +0200

    EH-70:changed smtp configs

--- apps/smtp_protocol/config/prod.exs
+++ apps/smtp_protocol/config/prod.exs
@@ -6,3 +6,17 @@ config :smtp_protocol, SmtpProtocol.RedisManager,
        host: "{$REDIS_HOST}",
        port: "{$REDIS_PORT}",
        pool_size: "{$REDIS_POOL_SIZE}"
+
+config :smtp_protocol, SmtpProtocol.RedisManager,
+       adapter: Bamboo.SMTPAdapter,
+       server: "{$SMTP_SERVER}",
+       hostname: "{$SMTP_HOSTNAME}",
+       port: 587,
+       username: "{$SMTP_USERNAME}",
+       password: "{$SMTP_PASSWORD}",
+       tls: :if_available, # can be `:always` or `:never`
+       allowed_tls_versions: [:tlsv1, :"tlsv1.1", :"tlsv1.2"], # or {:system, "ALLOWED_TLS_VERSIONS"} w/ comma seprated values (e.g. "tlsv1.1,tlsv1.2")
+       ssl: false, # can be `true`
+       retries: 1,
+       no_mx_lookups: false, # can be `true`
+       auth: :if_available
