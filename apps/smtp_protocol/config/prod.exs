617389ba060b2c870ebb985b452d40bb254d6537
ruslan moroz <r.moroz@skywell.software>
2019-02-13 09:56:30 +0200

    changed smtp prod config

--- apps/smtp_protocol/config/prod.exs
+++ apps/smtp_protocol/config/prod.exs
@@ -12,11 +12,11 @@ config :smtp_protocol, SmtpProtocol.RedisManager,
 
 config :smtp_protocol, SmtpProtocol.RedisManager,
        adapter: Bamboo.SMTPAdapter,
-       server: "{$SMTP_SERVER}",
-       hostname: "{$SMTP_HOSTNAME}",
+       server: "${SMTP_SERVER}",
+       hostname: "${SMTP_HOSTNAME}",
        port: 587,
-       username: "{$SMTP_USERNAME}",
-       password: "{$SMTP_PASSWORD}",
+       username: "${SMTP_USERNAME}",
+       password: "${SMTP_PASSWORD}",
        tls: :if_available, # can be `:always` or `:never`
        allowed_tls_versions: [:tlsv1, :"tlsv1.1", :"tlsv1.2"], # or {:system, "ALLOWED_TLS_VERSIONS"} w/ comma seprated values (e.g. "tlsv1.1,tlsv1.2")
        ssl: false, # can be `true`
